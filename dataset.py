80d732681bbb3a52ff0323492e4d2d129969495d
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-04-22 13:28:41 +0900

    Update dataset

--- dataset.py
+++ dataset.py
@@ -72,6 +72,41 @@ class HASC:
             raise NotImplementedError
 
 
+def load_hasc(path="./HASC_Apple_100/配布用/", window_size=512, stride=512):
+    """
+    Parameters
+    -------------
+    path:
+    window_size:
+    stride:
+
+    Returns
+    -------------
+    x_train, y_train, x_test, y_test
+    """
+    paths = [os.path.join(path, "dataset_{}".format(i)) for i in range(4)]
+
+    data = []
+    target = []
+
+    # Load data
+    for path in paths:
+        hasc = HASC(path)
+        data_, target_, _ = hasc.load(window_size, stride)
+
+        data.append(data_)
+        target.append(target_)
+
+    # train test
+    # dataset_0-2をtrain, dataset_3をtest
+    x_train = np.concatenate(data[:3])
+    y_train = np.concatenate(target[:3])
+    x_test = data[-1]
+    y_test = target[-1]
+
+    return x_train, y_train, x_test, y_test
+
+
 if __name__ == "__main__":
     hasc = HASC("./HASC_Apple_100/配布用/dataset_0")
     data, target, subject = hasc.load(512, 512)
