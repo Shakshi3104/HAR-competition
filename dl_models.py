c6f4c17459be208eec82c20ef27ed040fb2dcd9b
Shakshi3104 <watchosastrom3104@gmail.com>
2021-04-29 05:36:25 +0900

    Use tf.data.Dataset

--- dl_models.py
+++ dl_models.py
@@ -10,7 +10,7 @@ from tensoract.applications.vgg16 import VGG16
 from tensoract.applications.mobilenet import MobileNet
 from tensoract.applications.mobilenet_v2 import MobileNetV2
 from tensoract.applications.efficientnet import EfficientNetB0
-from tensoract.applications.pyramidnet import PyramidNet18
+from tensoract.applications.pyramidnet import PyramidNet18, PyramidNet101
 from tensoract.applications.resnet import ResNet18
 
 from tfxtend.keras.callbacks import ConfusionMatrixLogger, FMeasureLogger
@@ -48,15 +48,20 @@ if __name__ == "__main__":
     print(x_train.shape)
     print(y_test_.shape)
 
+    # dataset
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(len(x_train)).batch(batch)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test_)).batch(batch)
+
     # Load model
     models = {
-        "VGG16": VGG16(include_top=True, weights=None, input_shape=x_train.shape[1:]),
-        "VGG16-GAP": VGG16_GAP(include_top=True, weights=None, input_shape=x_train.shape[1:]),
+        # "VGG16": VGG16(include_top=True, weights=None, input_shape=x_train.shape[1:]),
+        # "VGG16-GAP": VGG16_GAP(include_top=True, weights=None, input_shape=x_train.shape[1:]),
         # "MobileNet": MobileNet(include_top=True, weights=None, input_shape=x_train.shape[1:]),
         # "MobileNetV2": MobileNetV2(include_top=True, weights=None, input_shape=x_train.shape[1:]),
-        # "EfficientNet B0": EfficientNetB0(include_top=True, weights=None, input_shape=x_train.shape[1:]),
-        "ResNet 18": ResNet18(include_top=True, weights=None, input_shape=x_train.shape[1:]),
-        "PyramidNet 18": PyramidNet18(include_top=True, weights=None, input_shape=x_train.shape[1:]),
+        "EfficientNet B0": EfficientNetB0(include_top=True, weights=None, input_shape=x_train.shape[1:]),
+        # "ResNet 18": ResNet18(include_top=True, weights=None, input_shape=x_train.shape[1:]),
+        # "PyramidNet 18": PyramidNet18(include_top=True, weights=None, input_shape=x_train.shape[1:]),
+        # "PyramidNet 101": PyramidNet101(include_top=True, weights=None, input_shape=x_train.shape[1:]),
     }
 
     for model_name, model in models.items():
@@ -76,9 +81,11 @@ if __name__ == "__main__":
         bench_callback = PerformanceLogger()
 
         # training
-        stack = model.fit(x=x_train, y=y_train, batch_size=batch, epochs=epochs,
-                          validation_data=(x_test, y_test_),
-                          verbose=1, callbacks=[cf_callback, fm_callback, bench_callback])
+        # stack = model.fit(x=x_train, y=y_train, batch_size=batch, epochs=epochs,
+        #                   validation_data=(x_test, y_test_),
+        #                   verbose=1, callbacks=[cf_callback, fm_callback, bench_callback])
+        stack = model.fit(train_ds, epochs=epochs, validation_data=test_ds, verbose=1,
+                          callbacks=[cf_callback, fm_callback, bench_callback])
 
         predict = model(x_test)
         predict = np.argmax(predict, axis=1)
