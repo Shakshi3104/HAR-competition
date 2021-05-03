f9d3a8b47496996b68dc923e47e949cda1bafda4
Shakshi3104 <watchosastrom3104@gmail.com>
2021-05-03 13:23:02 +0900

    a little update

--- utils.py
+++ utils.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score
-from tfxtend.metrics import confusion_error_matrix
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 
 
 def calc_metrics(y_true, y_pred, labels=["stay", "walk", "jog", "skip", "stUp", "stDown"]):
@@ -13,7 +12,7 @@ def calc_metrics(y_true, y_pred, labels=["stay", "walk", "jog", "skip", "stUp", 
     print("f1 : {}%".format(f_measure * 100))
 
     print("confusion matrix")
-    cf = confusion_error_matrix(y_pred, y_true, target_names=labels)
+    cf = confusion_matrix(y_pred, y_true, labels=labels)
     print(cf)
     print("")
 
